66a581e01d72bcd4cf4896559e1a9c25970d6b3a
hysKim1 <42089407+hysKim1@users.noreply.github.com>
2021-06-09 00:09:18 +0900

    2019/2020

---
+++ magic-8-ball-flutter-master/lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+            title: Text('답변해드립니다'), backgroundColor: Colors.blueGrey[900]),
+        body: Ball(),
+      ),
+    ),
+  );
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+}
